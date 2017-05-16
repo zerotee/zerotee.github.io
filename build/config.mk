3ee9dd4d0cf0e105dd330f7ad8a2fda9f17b6969
tirotekton <tirotekton@users.noreply.github.com>
2017-05-17 01:57:12 +0800

    Remove scripts/albums.js, use node-jq

--- build/config.mk
+++ build/config.mk
@@ -6,4 +6,7 @@ pub-dir           = ..
 assets-src-dir    = $(src-dir)/assets
 assets-pub-dir    = $(pub-dir)/assets
 node-modules-dir  = $(pub-dir)/node_modules
-PATH             := $(node-modules-dir)/.bin:$(PATH)
+
+PATH := $(node-modules-dir)/.bin:$(node-modules-dir)/node-jq/bin:$(PATH)
+
+export src-dir pub-dir assets-src-dir assets-pub-dir
--- build/site.mk
+++ build/site.mk
@@ -1,12 +1,17 @@
 # Custom targets
 # ==============
 
-# html
+read-prereq = $(file < $<)
+album-file  = $(html-pub-dir)/albums/%/index.html
 
 html: html-albums
 
 html-albums: Albums
-	@make --no-print-directory $$(cat $<)
+	@make --no-print-directory $(patsubst %,$(album-file),$(read-prereq))
+
+Albums: $(db-src-file)
+	jq -r '.albums[].id' $< | tr -d '\015' > $@
+	echo all >> $@
 
 $(html-pub-dir)/albums/%/index.html: \
   $(html-src-dir)/albums/album.html  \
@@ -15,8 +20,5 @@ $(html-pub-dir)/albums/%/index.html: \
 	scripts/render.js $< $@
 
 $(html-pub-dir)/about/index.html: $(html-src-dir)/about/about.md
-
-Albums: $(db-src-file)
-	scripts/albums.js > $@
 
 .PHONY: html-albums
