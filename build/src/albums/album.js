58ab709cf4836de375a69bdcd4ecd815d2642517
tirotekton <tirotekton@users.noreply.github.com>
2017-05-17 08:37:21 +0800

    Use same title for page and OpenGraph

--- build/src/albums/album.js
+++ build/src/albums/album.js
@@ -7,13 +7,15 @@ const album = index.albums.byId[albumId]
 const image = $context.config.baseURL + '/' + (
   $context.asset(`assets/img/meta/og-image-album-${albumId}.jpg`)
 )
+const title = 'Zero Tee | Tees' + (
+  (album && album.title) ? `: ${album.title}` : ''
+)
 
 const context = Lo.merge(index, {
+  title,
   config: {
     openGraph: {
-      title: 'Zero Tee | Tees' + (
-        (album && album.title) ? `: ${album.title}` : ''
-      ),
+      title,
       image
     },
     twitterCard: {
